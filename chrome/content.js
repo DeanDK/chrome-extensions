54bd9d878606463b0123a5f672c7d75e4d51b83f
DeanDK <deanbozic12@gmail.com>
2018-04-03 13:15:10 +0200

    Add boilerplate communication between background and content script

--- chrome/content.js
+++ chrome/content.js
@@ -1,5 +1,12 @@
-console.log("Chrome Extension Ready !");
-let paragraphs = document.getElementsByTagName("p");
-for (el of paragraphs) {
-  el.style["background-color"] = "#FF2f2f";
+// receive message from background script
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "hello") {
+    let paragraphs = document.getElementsByTagName("p");
+    for (el of paragraphs) {
+      el.style["background-color"] = "#FF2f2f";
+    }
+  }
 }
